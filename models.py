2280aaaf442f3053c67b993895763ed3acfa3034
g.dyachkov <g.dyachkov@inspector-cloud.com>
2023-05-15 22:20:40 +0200

    add_unique

--- models.py
+++ models.py
@@ -6,8 +6,8 @@ from database import Base
 class BaseUrl(Base):
     __tablename__ = 'baseurl'
     id = Column(Integer, primary_key=True, index=True)
-    long_url = Column(String)
-    short_url = Column(String)
+    long_url = Column(String(255), nullable=False, unique=True)
+    short_url = Column(String(255), nullable=False, unique=True)
 
     def to_json(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
